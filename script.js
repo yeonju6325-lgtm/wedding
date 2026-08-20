2c89b02aa737219e0b8f8ed539ce42de4c52c615
yeonju6325-lgtm <yeonju6325@gmail.com>
2026-08-20 13:35:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+const reveals = document.querySelectorAll(".reveal");
 
+const revealTexts = document.querySelectorAll(".reveal-text");
+
+
+const observer = new IntersectionObserver(
+    (entries) => {
+
+        entries.forEach((entry) => {
+
+            if (entry.isIntersecting) {
+
+                entry.target.classList.add("show");
+
+            }
+
+        });
+
+    },
+    {
+        threshold: 0.2
+    }
+);
+
+
+reveals.forEach((element) => {
+
+    observer.observe(element);
+
+});
+
+
+revealTexts.forEach((element) => {
+
+    observer.observe(element);
+
+});
